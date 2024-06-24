bc09fcf5049add280f80553632a10c6975c5b571
Krish Shah <2003kshah@gmail.com>
2024-06-24 01:25:28 -0400

    OTP firebase

--- firebase-admin-init.js
+++ firebase-admin-init.js
@@ -1,10 +1,18 @@
-// firebase-admin-init.js
 const admin = require('firebase-admin');
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Load the Firebase config from environment variables
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig)
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    // Replace literal \n with actual newlines
+    privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
+  }),
 });
 
 module.exports = admin;
-
