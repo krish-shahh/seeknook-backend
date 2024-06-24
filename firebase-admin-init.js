aa62394f47eb50b0672709954461fad83f3bac7c
Krish Shah <2003kshah@gmail.com>
2024-06-24 01:29:24 -0400

    OTP fix

--- firebase-admin-init.js
+++ firebase-admin-init.js
@@ -3,16 +3,22 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Load the Firebase config from environment variables
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: serviceAccount.project_id,
-    clientEmail: serviceAccount.client_email,
-    // Replace literal \n with actual newlines
-    privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
